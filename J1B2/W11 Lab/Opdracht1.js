7ed8ecd989ff3e2c06ec0b83abb4e520993699ea
kevin bouwmeester <koevoen420@gmail.com>
2019-11-13 10:18:40 +0100

    update week 9

---
+++ J1B2/W11 Lab/Opdracht1.js
@@ -0,0 +1,3 @@
+var weekdagen = ["maandag", " dinsdag", " woensdag", " donderdag", " vrijdag", " zaterdag", " zondag"]
+document.write("alle dagen van de week zijn <br>", weekdagen)
+document.write(weekdagen[])
